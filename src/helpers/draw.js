e0a0c1b6708f3a2f33957858c70808616c91499d
Ben Hallstein <benhall@brandwatch.com>
2019-02-26 11:35:30 +0000

    deliverables popups initial

--- src/helpers/draw.js
+++ src/helpers/draw.js
@@ -2,6 +2,15 @@
 // drawing helpers
 // -------------------------------
 
+const LEFT   = 1;
+const RIGHT  = 2;
+const TOP    = 3;
+const BOTTOM = 4;
+
+const NEAR = 10;
+const FAR  = 20;
+
+
 function line(context, from_x, from_y, to_x, to_y, color, width, line_cap, dash, alpha) {
   context.beginPath();
   context.strokeStyle = color;
@@ -76,10 +85,109 @@ function round_rect(context, x, y, w, h, radius, color) {
 }
 
 
+function popup_box(context, x, y, w, h, canvas_w, canvas_h, radius, color, measure) {
+  // draw a popup box focused on (x,y) - i.e. with a 'pointer triangle' pointing to that point
+  const triangle_height = radius * 1.35;
+  const triangle_width  = radius * 2.2;
+
+  const triangle_hpos__near = w / 3;
+  const triangle_hpos__far  = 2 * triangle_hpos__near;
+  const triangle_vpos__near = h / 4;
+  const triangle_vpos__far  = triangle_vpos__near;
+
+  let bounds = {
+    x: x - triangle_width/2 - triangle_hpos__near,
+    y: y + triangle_height,
+  };
+
+  const x_in_left_half = x <= canvas_w/2;
+  const y_in_top_half  = y <= canvas_h/2;
+
+  const triangle_box_side = (function() {
+    if (bounds.y + h > canvas_h) { return BOTTOM; }
+    if (bounds.x + w > canvas_w) { return RIGHT; }
+    if (bounds.x < 0) { return LEFT; }
+    return TOP;
+  })();
+
+  const triangle_near_or_far = (function() {
+    if (triangle_box_side === TOP)    { return x_in_left_half ? NEAR : FAR;  }
+    if (triangle_box_side === BOTTOM) { return x_in_left_half ? FAR  : NEAR; }
+    if (triangle_box_side === RIGHT)  { return y_in_top_half ? NEAR : FAR; }
+    return y_in_top_half ? FAR : NEAR;
+  })();
+
+  // Recalculate bounds now that we know where the triangle is positioned
+  bounds = {
+    [TOP + NEAR]: {  x: x - triangle_hpos__near,  y: y + triangle_height  },
+    [TOP + FAR]:  {  x: x - triangle_hpos__far,   y: y + triangle_height  },
+
+    [BOTTOM + NEAR]: {  x: x - triangle_width/2 - triangle_hpos__far,   y: y - triangle_height - h  },
+    [BOTTOM + FAR]:  {  x: x - triangle_width/2 - triangle_hpos__near,  y: y - triangle_height - h  },
+
+    [RIGHT + NEAR]: {  x: x - triangle_height - w,  y: y - triangle_width/2 - triangle_vpos__near  },
+    [RIGHT + FAR]:  {  x: x - triangle_height - w,  y: y - triangle_width/2 - triangle_vpos__far   },
+
+    [LEFT + NEAR]:  {  x: x + triangle_height,  y: y - triangle_width/2 - triangle_vpos__far  },
+    [LEFT + FAR]:   {  x: x + triangle_height,  y: y - triangle_width/2 - triangle_vpos__near },
+  }[triangle_box_side + triangle_near_or_far];
+
+  bounds.pointer_side = triangle_box_side;
+
+  if (measure) {
+    return bounds;
+  }
+
+
+  function draw_vertical_triangle(px, py, direction) {
+    const k = direction === TOP ? 1 : -1;
+
+    context.lineTo(px - triangle_width/2, py + k * triangle_height);
+    context.lineTo(px, py);
+    context.lineTo(px + triangle_width/2, py + k * triangle_height);
+  }
+
+  function draw_horizontal_triangle(px, py, direction) {
+    const k = direction === LEFT ? 1 : -1;
+
+    context.lineTo(px + k * triangle_height, py - triangle_width/2);
+    context.lineTo(px, py);
+    context.lineTo(px + k * triangle_height, py + triangle_width/2);
+  }
+
+  context.beginPath();
+  context.moveTo(bounds.x + radius, bounds.y);
+
+  if (triangle_box_side === TOP) { draw_vertical_triangle(x, y, triangle_box_side); }
+  context.arcTo(bounds.x+w, bounds.y, bounds.x+w, bounds.y+radius, radius);
+
+  if (triangle_box_side === RIGHT) { draw_horizontal_triangle(x, y, triangle_box_side); }
+  context.arcTo(bounds.x+w, bounds.y+h, bounds.x + w - radius, bounds.y+h, radius);
+
+  if (triangle_box_side === BOTTOM) { draw_vertical_triangle(x, y, triangle_box_side); }
+  context.arcTo(bounds.x, bounds.y+h, bounds.x, bounds.y + h - radius, radius);
+
+  if (triangle_box_side === LEFT) { draw_horizontal_triangle(x, y, triangle_box_side); }
+  context.arcTo(bounds.x, bounds.y, bounds.x+radius, bounds.y, radius);
+
+  context.fillStyle = color;
+  context.fill();
+  context.closePath();
+
+  return bounds;
+}
+
+
 export default {
   line,
   circle,
   text,
   round_rect,
+  popup_box,
+
+  LEFT,
+  RIGHT,
+  BOTTOM,
+  TOP,
 };
 
--- src/visualizations/roadmap.js
+++ src/visualizations/roadmap.js
@@ -1,14 +1,15 @@
+import AnimQueue from '../helpers/anim-queue';
 import helpers from '../helpers/helpers';
 import math from '../helpers/math';
-import {draw_line} from '../helpers/draw';
+import draw from '../helpers/draw';
 
 
 // Default opts
 // ----------------------------------------------------------
 
 const default_opts = {
-  start_date: () => first_of_this_month(),
-  end_date:   (g, opts) => roadmap.add_days(opts.start_date, n_days_this_year() - 1),
+  start: () => first_of_this_month(),
+  end:   (g, opts) => roadmap.add_days(opts.start, n_days_this_year() - 1),
 
   gridlines: false,
   gridlines_every: 'month',  // or 'week'
@@ -16,9 +17,19 @@ const default_opts = {
   gridlines_color: '#ccc',
   gridlines_style: 'solid',
 
-  stream_title_color: '#333',
-  stream_line_width: (g) => Math.min(8, g.h * 0.015),
+  stream_title_color:    '#333',
+  stream_line_width:     (g) => Math.min(8, g.h * 0.015),
   stream_title_fontsize: (g) => Math.min(16, g.h * 0.032),
+  stream_date_fontsize:  (g) => Math.min(15, g.h * 0.032),
+
+  popup_title_font: '"Helvetica Neue", Helvetica, Arial',
+  popup_title_weight: 900,
+  popup_title_color: 'black',
+  popup_title_size: 18,
+  popup_date_font: '"Helvetica Neue", Helvetica, Arial',
+  popup_date_weight: 900,
+  popup_date_color: 'aaa',
+  popup_date_size: 18,
 
   padding_h: 10,
 };
@@ -26,8 +37,8 @@ const default_opts = {
 const stream_defaults = {
   color: 'black',
 
-  start_date: null,
-  end_date: null,
+  start: null,
+  end:   null,
 };
 
 
@@ -40,14 +51,82 @@ function roadmap(el_canvas, streams, options) {
   // ----------------------------------------------------------
 
   el_canvas.addEventListener('BoggleChart:resize', draw_all);
+  el_canvas.addEventListener('mousemove', mouse_move);
+  el_canvas.addEventListener('mousedown', mouse_down);
+  el_canvas.addEventListener('mouseup', mouse_up);
+
   function unbind() {
     el_canvas.removeEventListener('BoggleChart:resize', draw_all);
   }
 
+  function mouse_enter(ev) {
+
+  }
+  function mouse_leave(ev) {
+
+  }
+  function mouse_move(ev) {
+    const d = deliverable_for_event(ev);
+    if (!d) {
+      el_canvas.style.cursor = 'default';
+      return;
+    }
+
+    el_canvas.style.cursor = 'pointer';
+  }
+  function mouse_down(ev) {
+    const d = deliverable_for_event(ev);
+    if (d) {
+      selection.deliverable = d;
+      draw_all();
+    }
+  }
+  function mouse_up(ev) {
+    const d = deliverable_for_event(ev);
+    if (!d) {
+      selection.deliverable = null;
+      draw_all();
+    }
+  }
+
+  function deliverable_for_event(ev) {
+    const p = helpers.get_offset_in_canvas(ev, el_canvas);
+    return deliverable_for_position(p);
+  }
+
+  function deliverable_for_position(p) {
+    const delivs = all_deliverables().filter(x => x.__position);
+    const rr = Math.pow(o.stream_line_width * 3, 2);
+
+    const i = helpers.find(delivs, d => {
+      const dx = d.__position[0] - p.x;
+      const dy = d.__position[1] - p.y;
+      return dx*dx + dy*dy <= rr;
+    });
+
+    return i > -1 ? delivs[i] : null;
+  }
+
+  function all_deliverables() {
+    return streams.reduce((accum, stream) => {
+      stream.deliverables.forEach(d => d.__color = stream.color);
+      return accum.concat(stream.deliverables || [ ]);
+    }, [ ]);
+  }
 
 
   // Drawing
   // ----------------------------------------------------------
+
+  const anim_queue = AnimQueue();
+  const highlight = {
+    item: 0,
+    progress: 1,
+  };
+
+  const selection = {
+    deliverable: null,   // index in roadmap
+  };
 
   function m(x) { return g.pr * x; }
 
@@ -67,8 +146,8 @@ function roadmap(el_canvas, streams, options) {
 
     // Month-aligned gridlines between start & end dates
     const w = g.w - 2 * o.padding_h;
-    const a = align_to_month_boundary(o.start_date);
-    const b = align_to_month_boundary(o.end_date, true);
+    const a = align_to_month_boundary(o.start);
+    const b = align_to_month_boundary(o.end, true);
 
     const n_months = (_ => {
       let n = (b.getFullYear() - a.getFullYear()) * 12 - a.getMonth() + 1 + b.getMonth();
@@ -79,9 +158,9 @@ function roadmap(el_canvas, streams, options) {
       const d = new Date(a);
       d.setMonth(a.getMonth() + i);
 
-      const x = o.padding_h + w * date_as_fraction_of_date_range(d, [o.start_date, o.end_date]);
+      const x = o.padding_h + w * date_as_fraction_of_date_range(d, [o.start, o.end]);
 
-      draw_line(c, x, g.h, x, 0, o.gridlines_color, m(o.gridlines_width));
+      draw.line(c, x, g.h, x, 0, o.gridlines_color, m(o.gridlines_width));
     }
   }
 
@@ -90,27 +169,35 @@ function roadmap(el_canvas, streams, options) {
     const vertical_space_per_stream = g.h / streams.length;
     const w = g.w - 2 * o.padding_h;
 
+    const min_alpha = 0.3;
+
     (streams || []).forEach((stream, i) => {
       const y = (i + 1) / (streams.length + 1) * g.h;
       const s = helpers.get_opts(stream_defaults, stream);
 
       const date_ranges = s.dates || [ ];
-      if (s.start_date && s.end_date) {
-        date_ranges.push({start_date: s.start_date, end_date: s.end_date});
+      if (s.start && s.end) {
+        date_ranges.push({start: s.start, end: s.end});
       }
 
-      date_ranges.forEach(r => {
-        const d_start = r.start_date;
-        const d_end   = r.end_date;
+      let alpha;
+      if (highlight.item === null)   { alpha = 1; }
+      else if (highlight.item === i) { alpha = 1; }
+      else                           { alpha = (1 - highlight.progress) * (1 - min_alpha) + min_alpha; }
 
-        const df_start = date_as_fraction_of_date_range(d_start, [o.start_date, o.end_date]);
-        const df_end   = date_as_fraction_of_date_range(d_end, [o.start_date, o.end_date]);
+      // Stream lines
+      date_ranges.forEach(r => {
+        const d_start = r.start;
+        const d_end   = r.end;
+
+        const df_start = date_as_fraction_of_date_range(d_start, [o.start, o.end]);
+        const df_end   = date_as_fraction_of_date_range(d_end, [o.start, o.end]);
 
         const x1 = o.padding_h + w * df_start;
         const x2 = o.padding_h + w * df_end;
+        const x2_actual = (x2 - x1) * math.ease_out_cubic_simple(s.draw_progress) + x1;
 
-        // Main lines
-        draw_line(c, x1, y, x2, y, s.color, m(o.stream_line_width), 'round');
+        draw.line(c, x1, y, x2_actual, y, s.color, m(o.stream_line_width), 'round', undefined, alpha);
       });
 
 
@@ -127,23 +214,97 @@ function roadmap(el_canvas, streams, options) {
 
       // Deliverables
       (s.deliverables || [ ]).forEach(deliv => {
-        const df = date_as_fraction_of_date_range(deliv.date, [o.start_date, o.end_date]);
+        const df = date_as_fraction_of_date_range(deliv.date, [o.start, o.end]);
         const x = o.padding_h + w * df;
 
         const r = o.stream_line_width;
-        const stroke = 5/8 * o.stream_line_width;
+        const rk = deliv === selection.deliverable ? 1.5 : 1;
+        const stroke = 5/8 * o.stream_line_width * rk;
+
+        deliv.__position = [x, y];
 
         c.beginPath();
-        c.arc(x, y, m(r), 0, 2 * Math.PI);
+        c.arc(x, y, m(r) * rk, 0, 2 * Math.PI);
         c.fillStyle = 'white';
         c.fill();
 
+        c.globalAlpha = alpha;
         c.strokeStyle = s.color;
-        c.lineWidth = m(stroke);
+        c.lineWidth   = m(stroke);
         c.stroke();
         c.closePath();
+        c.globalAlpha = 1;
       });
     });
+  }
+
+
+  function draw_selected_deliverable() {
+    if (!selection.deliverable) {
+      return;
+    }
+
+    const d = selection.deliverable;
+    const p = d.__position;
+
+    const fontsize__title = m(o.popup_title_size);
+    const fontsize__date = m(o.popup_date_size);
+    const font__title = `${o.popup_title_weight} ${fontsize__title}px ${o.popup_title_font}`;
+    const font__date  = `${o.popup_date_weight} ${fontsize__date}px ${o.popup_date_font}`;
+
+    // Calculate popup size
+    const targs__title = [
+      c, d.name || '[no name]', 0, 0,  o.popup_title_color, font__title, 0, 'top'
+    ];
+    const targs__date = [
+      c, friendly_date_string(d.date), 0, fontsize__title * 1.4, o.popup_date_color, font__date, 0, 'top'
+    ];
+    const textsize__title = draw.text(...targs__title, true);
+    const textsize__date  = draw.text(...targs__date, true);
+    const txt_w = Math.max(textsize__title.width, textsize__date.width);
+
+    const popup_padding = m(28);
+    const popup_stroke = m(4);
+    const popup_radius = m(13);
+    const popup_w = popup_padding + txt_w + popup_padding*3;
+    const popup_h = 2*popup_padding + 1.5 * fontsize__title + fontsize__date;
+
+
+    const popup_args = [ c, p[0], p[1] + m(4), popup_w, popup_h, g.w, g.h, popup_radius, 'white' ];
+    const bounds = draw.popup_box(...popup_args, true);
+    const stroke_bounds = (_ => {
+      const s = bounds.pointer_side;
+      const out = Object.assign({}, bounds);
+      out.y += s === draw.TOP ? popup_stroke : 0;
+      out.y += s === draw.BOTTOM ? -popup_stroke : 0;
+      out.x += s === draw.LEFT ? popup_stroke : 0;
+      out.x += s === draw.RIGHT ? -popup_stroke : 0;
+      return out;
+    })();
+
+    function shadow_on() {
+      c.save()
+      c.shadowColor = '#cecece';
+      c.shadowBlur = m(10);
+      c.shadowOffsetY = m(2);
+    }
+    function shadow_off() {
+      c.restore();
+    }
+
+    draw.round_rect(c, stroke_bounds.x, stroke_bounds.y, popup_w, popup_h, popup_radius, d.__color);
+    shadow_on();
+    draw.popup_box(...popup_args);
+    shadow_off();
+
+    targs__title[2] = bounds.x + popup_padding;
+    targs__title[3] = bounds.y + popup_padding;
+
+    targs__date[2] = bounds.x + popup_padding;
+    targs__date[3] += bounds.y + popup_padding;
+
+    draw.text(...targs__title);
+    draw.text(...targs__date);
   }
 
 
@@ -155,11 +316,38 @@ function roadmap(el_canvas, streams, options) {
     c.clearRect(0, 0, g.w, g.h);
     draw_gridlines();
     draw_streams();
+    draw_selected_deliverable();
+  }
+
+
+  function do_draw() {
+    anim_queue.reset();
+    anim_queue.add(animtask__initial());
+    anim_queue.start();
+  }
+
+
+  function animtask__initial() {
+    let stream_draw_progress = 0;
+
+    return function() {
+      const time_progress = math.delayed_time_series(streams.length, 0.6, 0.2, stream_draw_progress);
+      streams.forEach((s, i) => s.draw_progress = time_progress[i]);
+
+      draw_all();
+
+      stream_draw_progress = Math.min(stream_draw_progress + 0.03, 1);
+      if (stream_draw_progress === 1) {
+        anim_queue.finishTask();
+        streams.forEach(s => s.draw_progress = 1);
+        draw_all();
+      }
+    };
   }
 
 
   return {
-
+    draw: do_draw,
   };
 }
 
@@ -226,6 +414,10 @@ function date_as_fraction_of_date_range(d, range) {
   const b = range[1].getTime();
 
   return (x - a) / (b - a);
+}
+
+function friendly_date_string(d) {
+  return `${d.toLocaleDateString('en-EN', {day: 'numeric', month: 'long'})}, ${d.getFullYear()}`;
 }
 
 
